e592c07f7f58306a51393f22b90be861af103b79
rajraju9223 <rajraju9223@gmail.com>
2025-06-23 03:00:57 +0530

    db test commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Student;
+
 class StudentController extends Controller
 {
     //
     function list(){
-        return "list function called";
+        return Student::all();
     }
 }
